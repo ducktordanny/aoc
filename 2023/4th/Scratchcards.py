ae4f058f6a44caf9cc56a8fb677f65a389e673fa
Daniel Lazar <lazardani0112@gmail.com>
2023-12-04 22:08:58 +0100

    2023/4th: second part of Scratchcards

--- 2023/4th/Scratchcards.py
+++ 2023/4th/Scratchcards.py
@@ -21,3 +21,17 @@ for list_index, winning_numbers in enumerate(winning_numbers_list):
     part1_result += points
 
 print('part 1:', part1_result)
+
+# Most efficient code ever:
+copies_of_cards_list = [0] * len(scratch_cards)
+for list_index, winning_numbers in enumerate(winning_numbers_list):
+    for i in range(copies_of_cards_list[list_index] + 1):
+        copies = 0
+        for winner in winning_numbers:
+            if winner in own_numbers_list[list_index]:
+                copies += 1
+        for j in range(copies):
+            copies_of_cards_list[list_index + j + 1] += 1
+
+part2_result = sum(copies_of_cards_list) + len(scratch_cards)
+print('part 2:', part2_result)
